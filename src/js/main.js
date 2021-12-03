0e936cb288f3c35e639b429b6638eaf0c345d733
Quynh Mai Nguyen <maiqn92@googlemail.com>
2021-12-03 16:27:29 +0100

    REDESIGN

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+// MOBILE MENU BUTTON
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+})
+
