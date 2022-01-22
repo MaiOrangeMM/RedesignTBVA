8239a0e64a0ef2fb0de4508a2016ca81c17d7ad9
Mai Orange <42119515+MaiOrangeMM@users.noreply.github.com>
2022-01-22 19:44:55 +0100

    adding reCAPTCHA

--- src/js/main.js
+++ src/js/main.js
@@ -1,74 +1,81 @@
 const lines = document.querySelectorAll(".line");
 const social = document.querySelector("#socialtab");
 // FULLPAGE JS
-new fullpage('#fullpage', {
-	//options here
-    licenseKey: 'YB6B09247-BFD74F6D-80FC8055-F23750CA',
-    autoScrolling: true,
-    anchors: ['home', 'about', 'portfolio', 'services', 'contact'],
-    menu:'#menu',
-    scroll: true,
-    fitToSection: true,
-    css3: true,
-    responsiveWidth: 1200,
+new fullpage("#fullpage", {
+  //options here
+  licenseKey: "YB6B09247-BFD74F6D-80FC8055-F23750CA",
+  autoScrolling: true,
+  anchors: ["home", "about", "portfolio", "services", "contact"],
+  menu: "#menu",
+  scroll: true,
+  fitToSection: true,
+  css3: true,
+  responsiveWidth: 1200,
 });
 
 // MOBILE MENU BUTTON
-const menuBtn = document.querySelector('.menu-btn');
-const menuList = document.querySelector('.menulinks');
+const menuBtn = document.querySelector(".menu-btn");
+const menuList = document.querySelector(".menulinks");
 let menuOpen = false;
 
-menuBtn.addEventListener('click', () => {
-    if (!menuOpen) {
-        menuBtn.classList.add('open');
-        menuOpen = true;
-        menuList.classList.remove('d-none');
-    } else {
-        menuBtn.classList.remove('open');
-        menuOpen = false;
-        menuList.classList.add('d-none');
-    }
-})
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+    menuList.classList.remove("d-none");
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+    menuList.classList.add("d-none");
+  }
+});
 
 /* CAROUSEL */
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
 
-const track = document.querySelector('.track');
+const track = document.querySelector(".track");
 
-let carouselWidth = document.querySelector('.carousel-container').offsetWidth;
+let carouselWidth = document.querySelector(".carousel-container").offsetWidth;
 
-window.addEventListener('resize', () => {
-    carouselWidth = document.querySelector('.carousel-container').offsetWidth;
-})
+window.addEventListener("resize", () => {
+  carouselWidth = document.querySelector(".carousel-container").offsetWidth;
+});
 
 let index = 0;
 
-next.addEventListener('click', () => {
+next.addEventListener("click", () => {
   index++;
-  prev.classList.add('show');
+  prev.classList.add("show");
   track.style.transform = `translateX(-${index * 900}px)`;
-  if (track.offsetWidth - (index * 900) < carouselWidth) {
-    next.classList.add('hide');
+  if (track.offsetWidth - index * 900 < carouselWidth) {
+    next.classList.add("hide");
   }
-})
+});
 
-prev.addEventListener('click', () => {
+prev.addEventListener("click", () => {
   index--;
-  next.classList.remove('hide');
+  next.classList.remove("hide");
   if (index === 0) {
-    prev.classList.remove('show');
+    prev.classList.remove("show");
   }
   track.style.transform = `translateX(-${index * 900}px)`;
 });
 
-
 // LIGHTBOX
 lightbox.option({
-  'resizeDuration': 200,
-  'wrapAround': true,
-  'alwaysShowNavOnTouchDevices': true,
-  'fitImagesInViewport': true,
-  'showImageNumberLabel': true,
-  'albumLabel': "Image %1 of %2",
-})
+  resizeDuration: 200,
+  wrapAround: true,
+  alwaysShowNavOnTouchDevices: true,
+  fitImagesInViewport: true,
+  showImageNumberLabel: true,
+  albumLabel: "Image %1 of %2",
+});
+
+$recaptcha = $_POST["g-recaptcha-response"];
+$res = reCaptcha($recaptcha);
+if ($res["success"]) {
+  // Send email
+} else {
+  // Error
+}
